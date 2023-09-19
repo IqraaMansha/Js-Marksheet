cfedde14fc813cbdeef8fed6d794d4ea04323c31
Iqra Mansha <iqramansha78u@gmail.com>
2023-09-19 23:35:02 +0500

    First Commit

---
+++ script.js
@@ -0,0 +1,134 @@
+
+
+var name = "Faiza";
+var math = 50;
+var eng = 40;
+var urdu = 50;
+var total = 300;
+var Total = 150;
+var Percentage = Total / total * 140;
+
+
+
+var name2 = "Sara";
+var math2 = 35;
+var eng2 = 45;
+var urdu2 = 55;
+var total2 = 305;
+var Total2 = 200;
+var Percentage2 = Total2 / total2 * 140;
+
+
+
+
+var name3 = "Anees";
+var math3 = 30;
+var eng3 = 40;
+var urdu3 = 50;
+var total3 = 310;
+var Total3 = 200;
+var Percentage3 = Total3 / total3 * 140;
+
+
+
+var name4 = "Alyan";
+var math4 = 30;
+var eng4 = 40;
+var urdu4 = 50;
+var total4 = 330;
+var Total4 = 200;
+var Percentage4 = Total4 / total4 * 140;
+
+
+
+
+var name5 = "Iqra";
+var math5 = 30;
+var eng5 = 40;
+var urdu5 = 50;
+var total5 = 350;
+var Total5 = 200;
+var Percentage5 = Total5 / total5 * 140;
+
+
+
+
+
+document.write(
+    "<center>" +
+    "<table border = '3' >" +
+    "<tr> " +
+    "<th>" + "Name" + "</th>" +
+    "<th>" + "Math" + "</th>" +
+    "<th>" + "Eng" + "</th>" +
+    "<th>" + "Urdu" + "</th>" +
+    "<th>" + "Total" + "</th>" +
+    "<th>" + "Percentage" + "</th>" +
+    "</tr>" +
+
+    "<tr>" +
+    "<td>" + name + "</td>" +
+    "<td>" + math + "</td>" +
+    "<td>" + eng + "</td>" +
+    "<td>" + urdu + "</td>" +
+    "<td>" + total + "</td>" +
+    "<td>" + Percentage + "</td>" +
+
+    "</tr>" +
+
+
+    "<tr>" +
+    "<td>" + name2 + "</td>" +
+    "<td>" + math2 + "</td>" +
+    "<td>" + eng2 + "</td>" +
+    "<td>" + urdu2 + "</td>" +
+    "<td>" + total2 + "</td>" +
+    "<td>" + Percentage2 + "</td>" +
+
+
+    "</tr>" +
+
+
+    "<tr>" +
+    "<td>" + name3 + "</td>" +
+    "<td>" + math3 + "</td>" +
+    "<td>" + eng3 + "</td>" +
+    "<td>" + urdu3 + "</td>" +
+    "<td>" + total3 + "</td>" +
+    "<td>" + Percentage3 + "</td>" +
+
+    "</tr>" +
+
+
+
+    "<tr>" +
+    "<td>" + name4 + "</td>" +
+    "<td>" + math4 + "</td>" +
+    "<td>" + eng4 + "</td>" +
+    "<td>" + urdu4 + "</td>" +
+    "<td>" + total4 + "</td>" +
+    "<td>" + Percentage4 + "</td>" +
+
+    "</tr>" +
+
+
+    "<tr>" +
+    "<td>" + name5 + "</td>" +
+    "<td>" + math5 + "</td>" +
+    "<td>" + eng5 + "</td>" +
+    "<td>" + urdu5 + "</td>" +
+    "<td>" + total5 + "</td>" +
+    "<td>" + Percentage5 + "</td>" +
+
+    "</tr>" +
+
+
+    "<table>" +
+    "</center>"
+)
+
+
+
+
+alert(name5 + " " + Percentage5);
+alert(name2 + " " + Percentage2);
